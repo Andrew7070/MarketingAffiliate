dada8c85725ec22e70a40deab47b4c156d374757
Andrew Asare-Nyako <asare_nyako@yahoo.com>
2021-12-12 10:39:32 +0000

    Orders routing error fixed

--- src/app/orders/orders-routing.module.ts
+++ src/app/orders/orders-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OrderComponent } from './order/order.component';
 import { OrdersComponent } from './orders/orders.component';
+import { OrderEditComponent } from './order-edit/order-edit.component';
 
 const routes: Routes = [
 { path: '', component: OrdersComponent },
 { path: ':id', component: OrderComponent },
-{ path: ':id/edit', component: OrderEdit }
+{ path: ':id/edit', component: OrderEditComponent }
 ];
 
 @NgModule({
