090de6bc799856057b9f994e5db8bac04208d43a
Andrew Asare-Nyako <asare_nyako@yahoo.com>
2021-12-12 14:58:48 +0000

    OAuth2 implementation started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { JwksValidationHandler, OAuthService } from 'angular-oauth2-oidc';
+import { authCodeFlowConfig } from './core/services/sso.config';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor (private oauthService:OAuthService){
+    this.configureSingleSignOn();
+  }
+
+  configureSingleSignOn(){
+    this.oauthService.configure(authCodeFlowConfig);
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+
+  }
+
+  login(){
+    this.oauthService.initCodeFlow();
+
+  }
+
+  logout(){
+    this.oauthService.logOut();
+  }
+
   title = 'marketing-affiliate';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { HttpClientModule } from '@angular/common/http';
+import { OAuthModule } from 'angular-oauth2-oidc';
+
+
 //import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
-import { ReactiveFormsModule } from '@angular/forms';
+
+
 
 
 @NgModule({
@@ -14,6 +21,9 @@ import { ReactiveFormsModule } from '@angular/forms';
     //WelcomeComponent
   ],
   imports: [
+    HttpClientModule,
+    OAuthModule.forRoot(),
+
     AppRoutingModule,
     BrowserModule,
     ReactiveFormsModule,
